ad974d5daaa0233f96da3b1d1d06405869f743cd
Nicholas Bassey <nickyawat@gmail.com>
2021-05-21 20:29:06 +0100

    created first function

---
+++ app.js
@@ -0,0 +1,9 @@
+// A function that converts Degree Farenheit to Degree Celsius
+
+function convertFahrToCelsius (degF) {
+    let degC = (degF - 32) * 5/9;
+    result = degC
+
+}
+convertFahrToCelsius(55)
+console.log(result)
